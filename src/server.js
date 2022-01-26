b4a01fd4b15d548b542c70f5dfc3dbb329a9c034
kisublee <yigi2021@gmail.com>
2022-01-26 08:58:13 -0500

    Fix gitignore.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 // Pull in app.js.
 const app = require("./app");
-
+const cors = require("cors");
+app.use(cors());
 // Pull MongoDB
 require("./db/mongoose");
 
@@ -11,6 +12,6 @@ const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+app.listen(PORT || 5000, () => {
   console.log("Successfully listening on: " + PORT);
 });
